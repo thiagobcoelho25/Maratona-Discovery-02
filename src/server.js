9acbceb3e1e5bdf24419b6fb77519a35e0613c42
Thiago Ribeiro <thiagobcoelho25@gmail.com>
2021-04-03 14:27:41 -0300

    Aula 03

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const server  = express();
 const routes = require("./routes");
+const path = require('path')
 
 //Usando template engine
 server.set('view engine', 'ejs')
+
+//mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
 
 //Habilitar arquivos statics
 server.use(express.static("public"))
